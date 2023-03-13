3847cbbf9c6d1d1c9dfbf6ec9a10b3f8d7a3e5c2
MathisPastreNelson <shiray@hotmail.fr>
2023-03-13 20:01:52 +0100

    Amélioration du front les 3 sous composant sont en place

--- src/Movies.js
+++ src/Movies.js
@@ -1,5 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Movies() {
-  return <div>Movie</div>;
+  // Les states
+  const [movies, setMovies] = useState([]);
+  // Le rendu
+  return (
+    <div className="movie_box">
+      <p>Movies will come here</p>
+    </div>
+  );
 }
