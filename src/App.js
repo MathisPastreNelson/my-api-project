61cc795608c4e3f63fb9537be2557692943381cd
MathisPastreNelson <shiray@hotmail.fr>
2023-03-13 14:44:54 +0100

    Base de react classique

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./styles/index.css";
+import Header from "./Header";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <p>Voici le commencement du front-end</p>
+      <Footer />
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Importation du Header</h1>
+    </header>
+  );
+}
