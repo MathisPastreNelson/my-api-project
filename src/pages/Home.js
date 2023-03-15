afc36feab936b21f69402e51a4bc084439cc57e1
MathisPastreNelson <shiray@hotmail.fr>
2023-03-15 17:52:34 +0100

    On recoit les donnée sur les deux type de pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,16 +6,17 @@ import Footer from "../components/Footer";
 
 const API_URL =
   "https://api.themoviedb.org/3/movie/popular?api_key=f8aa98bfbb1c2940f9b235241e070eee&language=fr";
+
 export default function Movies() {
   // Les states
   const [movies, setMovies] = useState([]);
+
   // UseEffect
   useEffect(() => {
     fetch(API_URL)
       .then((res) => res.json())
       .then((data) => {
         setMovies(data.results);
-        console.log(movies);
       });
   }, [movies]);
 
--- src/pages/SpecificMovie.js
+++ src/pages/SpecificMovie.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 // Les composants
@@ -6,17 +7,41 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 export default function Movies() {
-  /* Récupération de l'ID grâce à l'URL */
   let { id } = useParams();
+
+  /* Récupération de l'ID grâce à l'URL */
+  const API_URL = `https://api.themoviedb.org/3/movie/${id}?api_key=f8aa98bfbb1c2940f9b235241e070eee&language=fr`;
+
+  // Les states
+  const [movies, setMovies] = useState([]);
+
+  // UseEffect
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data);
+      });
+  }, [movies, API_URL]);
 
   // Le rendu
   return (
     <div>
       <Header />
       <div className="movie_container">
-        <h2>Films récents populaires :</h2>
-        <div className="movie_box">
-          <p>test</p>
+        <div>
+          <h2>{movies.title}</h2>
+          <img
+            className="movie_img"
+            src={`https://image.tmdb.org/t/p/w500/${movies.poster_path}`}
+            alt="Affiche du film"
+          />
+          <p>{movies.overview}</p>
+          <p>
+            Budget :{" "}
+            {movies.budget > 0 ? movies.budget / 1000000 : "Non renseigné"}
+            {movies.budget > 0 && " M"}
+          </p>
         </div>
         <a href="../my-api-project">Retour à la page d'index</a>
       </div>
