0b4c0912d31b72472044b55e04a825a50bacc4c3
MathisPastreNelson <shiray@hotmail.fr>
2023-03-19 02:48:59 +0100

    préparation de mes genres préférés

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 // Les composants
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { NavLink } from "react-router-dom";
 
 const API_URL =
   "https://api.themoviedb.org/3/movie/popular?api_key=f8aa98bfbb1c2940f9b235241e070eee&language=fr";
@@ -24,15 +25,37 @@ export default function Movies() {
   return (
     <div>
       <Header />
+      <nav className="navLink_Bar">
+        {" "}
+        <NavLink
+          className="movie_article"
+          key="Horreur"
+          to={"/my-api-project/horror"}>
+          Comédie
+        </NavLink>
+        <NavLink
+          className="movie_article"
+          key="Comédie"
+          to={"/my-api-project/comedy"}>
+          Horreur
+        </NavLink>
+        <NavLink
+          className="movie_article"
+          key="Généraliste"
+          to={"/my-api-project/global"}>
+          Généraliste
+        </NavLink>
+      </nav>
+
       <div className="movie_container">
         <h2 className="movie_introduce">Films récents populaires :</h2>
         <div className="movie_box">
           {/* Le .map qui va récupéré l'enssemble des données de API_URL */}
           {movies.map((movie) => (
-            <a
-              href={`my-api-project/${movie.id}`}
+            <NavLink
               className="movie_article"
-              key={movie.id}>
+              key={movie.id}
+              to={"/my-api-project/" + movie.id}>
               <h3>{movie.title}</h3>
               <img
                 className="movie_img"
@@ -44,7 +67,7 @@ export default function Movies() {
             ) : (
               <p>Description non disponible</p>
             )} */}
-            </a>
+            </NavLink>
           ))}
         </div>
       </div>
