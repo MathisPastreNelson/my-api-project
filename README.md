45884ac8ee71e17d6f05c84dcbf7cae4d819f2fc
MathisPastreNelson <shiray@hotmail.fr>
2023-03-21 00:12:15 +0100

    Interaction avec le genre comedy OK

--- README.md
+++ README.md
@@ -4,3 +4,84 @@ API_KEY = f8aa98bfbb1c2940f9b235241e070eee
 
 TOKEN_READ_API =
 eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmOGFhOThiZmJiMWMyOTQwZjliMjM1MjQxZTA3MGVlZSIsInN1YiI6IjY0MGRkMGZiMzIzZWJhMDBjNWVhOWQ5OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RsCdUmgMAoWnXl7ut97kya1xezlDxc9Jxx9Up0HsJrM
+
+{
+"genres": [
+{
+"id": 28,
+"name": "Action"
+},
+{
+"id": 12,
+"name": "Adventure"
+},
+{
+"id": 16,
+"name": "Animation"
+},
+{
+"id": 35,
+"name": "Comedy"
+},
+{
+"id": 80,
+"name": "Crime"
+},
+{
+"id": 99,
+"name": "Documentary"
+},
+{
+"id": 18,
+"name": "Drama"
+},
+{
+"id": 10751,
+"name": "Family"
+},
+{
+"id": 14,
+"name": "Fantasy"
+},
+{
+"id": 36,
+"name": "History"
+},
+{
+"id": 27,
+"name": "Horror"
+},
+{
+"id": 10402,
+"name": "Music"
+},
+{
+"id": 9648,
+"name": "Mystery"
+},
+{
+"id": 10749,
+"name": "Romance"
+},
+{
+"id": 878,
+"name": "Science Fiction"
+},
+{
+"id": 10770,
+"name": "TV Movie"
+},
+{
+"id": 53,
+"name": "Thriller"
+},
+{
+"id": 10752,
+"name": "War"
+},
+{
+"id": 37,
+"name": "Western"
+}
+]
+}
--- src/pages/Comedy.js
+++ src/pages/Comedy.js
@@ -1,27 +1,60 @@
-import React from "react";
-import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 
 // Les composants
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 export default function Comedy() {
-  const fetchGenre = async () => {
-    await axios.get(
-      "https://api.themoviedb.org/3/genre/movie/list?api_key=f8aa98bfbb1c2940f9b235241e070eee&language=fr".then(
-        (res) => {
-          const data = res.data;
-          this.setState({ data });
-        }
-      )
-    );
-  };
+  // Les states
+  const [movies, setMovies] = useState([]);
+  const [moviePage, setMoviePage] = useState(25);
+  const API_URL = `https://api.themoviedb.org/3/discover/movie?api_key=f8aa98bfbb1c2940f9b235241e070eee&language=fr_fr&sort_by=release_date.desc&page=${moviePage}&with_genres=35`;
 
-  console.log(fetchGenre());
+  // UseEffect
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.results);
+      });
+  }, [movies, API_URL]);
+
   return (
     <div>
       <Header />
-      <div>Comedy</div>
+      <div className="movie_container">
+        <h2 className="movie_introduce">Comédies récentes :</h2>
+        <nav className="navLink_Bar">
+          <NavLink className="movie_article" key="less" to={setMoviePage - 1}>
+            Sortie plus récente
+          </NavLink>
+          <NavLink className="movie_article" key="plus" to={setMoviePage + 1}>
+            Sortie plus ancienne
+          </NavLink>
+        </nav>
+        <div className="movie_box">
+          {/* Le .map qui va récupéré l'enssemble des données de API_URL */}
+          {movies.map((movie) => (
+            <NavLink
+              className="movie_article"
+              key={movie.id}
+              to={"/my-api-project/" + movie.id}>
+              <h3>{movie.title}</h3>
+              <img
+                className="movie_img"
+                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                alt="Affiche du film"
+              />
+              {/* {movie.overview ? (
+              <p>{movie.overview}</p>
+            ) : (
+              <p>Description non disponible</p>
+            )} */}
+            </NavLink>
+          ))}
+        </div>
+      </div>
       <Footer />
     </div>
   );
